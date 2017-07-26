14a733c5d6601f81b9eb6ad6725190541a5e0923
Ruhee D'cunha <rdcunha2@vt.edu>
2017-07-26 15:30:07 -0400

    auto test scf code

--- tests/test_scf.py
+++ tests/test_scf.py
@@ -1,4 +1,19 @@
 import pytest
+import scf_11
+import numpy as np
+import psi4
 
 def test_scf():
-    assert True
+    mole = psi4.geometry("""
+    O
+    H 1 1.1
+    H 1 1.1 2 104
+    """)
+    mole.update_geometry()
+    e_conv = 1.e-6
+    d_conv = 1.e-6
+    nel = 5
+    bas = psi4.core.BasisSet.build(mole, target="aug-cc-PVDZ")
+    mints = scf_11.get_mints(bas)
+    E_total = scf_11.scf(mints, e_conv, d_conv, nel)
+    assert np.allclose(psi4_energy, E_total)
