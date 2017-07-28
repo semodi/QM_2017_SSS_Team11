4319b521517da49d8d7ae66affd2a4753cac2792
Kate Lebold <kml5480@psu.edu>
2017-07-28 17:27:28 -0400

    Added working MP2 and tests

--- scf_11/mp2.py
+++ scf_11/mp2.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def mp2(molecule, SCS_mode):
+def mp2(molecule, SCS_mode, E_SCF):
     g = molecule.get_ao_eri()
     C = molecule.C
     eps = molecule.eps
@@ -17,15 +17,6 @@ def mp2(molecule, SCS_mode):
     iajs = np.einsum("va,ivjs->iajs", C, ivjs)
     iajb = np.einsum("sb,iajs->iajb", C, iajs)
 
-    # Calculate (ib|ja) = SS_num
-    # ivls and ivjs as above
-    ibjs = np.einsum("vb, ivjs->ibjs", C, ivjs)
-    ibja = np.einsum("sa,ibjs->ibja", C, ibjs)
-
-    # Calculate the numerators (ia|jb)(ia|jb) = OS_num and [(ia|jb) - (ib|ja)](ia|jb) = SS_num
-    OS_num = iajb.T @ iajb
-    SS_num = (iajb - ibja) @ iajb
-
     # Loop over all i, a, j, and b to calculate the energies:
     # nel = number of doubly-occupied orbitals
     # len(C) = total number of orbitals
@@ -37,13 +28,17 @@ def mp2(molecule, SCS_mode):
         for j in range(nel):
             for a in range(nel, len(C)):
                 for b in range(nel, len(C)):
-                    E_MP2OS += OS_num[i, j, a, b]/(eps[i]+eps[j]-eps[a]-eps[b])
-                    E_MP2SS += SS_num[i, j, a, b]/(eps[i]+eps[j]-eps[a]-eps[b])    
+                    E_MP2OS += (iajb[i, a, j, b]*iajb[i, a, j, b])/(eps[i]+eps[j]-eps[a]-eps[b])
+                    E_MP2SS += ((iajb[i, a, j, b] - iajb[i, b, j, a])*iajb[i, a, j, b])/(eps[i]+eps[j]-eps[a]-eps[b])
 
-    # Determine the energy based on whether it should be spin-component scaled (SCS_mode) or not
+
+    # Determine the MP2 correction based on whether it should be spin-component scaled (SCS_mode) or not
     if(SCS_mode):
         E_MP2 = (1.0/3.0)*E_MP2SS + (6.0/5.0)*E_MP2OS
     else:
-        E_MP2 = E_MP2OS - E_MP2SS
+        E_MP2 = E_MP2OS + E_MP2SS
 
-    return E_MP2
+    # Calculate the total energy = E_SCF + E_MP2
+    E_tot = E_SCF + E_MP2
+
+    return E_tot
